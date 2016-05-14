cfa8a2ae4f11692c839efd698ed04165322af152
Hien Dang <hienpd@outlook.com>
2016-05-14 15:18:32 -0700

    Current color indicator works

--- js/app.js
+++ js/app.js
@@ -1,9 +1,7 @@
 var canvas = document.getElementById('canvas');
 var brushColor;
 
-// Draw the canvas based on the height and width given.
-// Height determines the number of rows.
-// Width determines how many pixels to put in each row.
+/* Draw the canvas based on the height and width given. Height determines the number of rows. Width determines how many pixels to put in each row. */
 
 var drawCanvas = function (width, height) {
   for (var row = 0; row < height; row++) {
@@ -26,17 +24,21 @@ var drawCanvas = function (width, height) {
   }
 };
 
-// Pick a color. Clicking on a color saves the class name.
+/* Allow user to pick a color. Clicking on a color saves the class name. Add event listeners to all the color panels on the palette except the last one, which is the current color indicator. Add brush color class to indicator. */
 
 var colorPicker = function () {
   var palette = document.getElementById('palette');
   var color = palette.getElementsByClassName('color');
-  for (var i = 0; i < color.length; i++) {
+  var currentColor = document.getElementsByClassName('current')[0];
+  for (var i = 0; i < color.length - 1; i++) {
     color[i].addEventListener('click', function(event) {
-      brushColor = event.target.className.slice(6);
+    brushColor = event.target.className.slice(6);
+    currentColor.className = 'color current ' + brushColor;
     });
   }
 };
+
+/* Show the current brush color on the palette. */
 
 drawCanvas(32, 24);
 colorPicker();
