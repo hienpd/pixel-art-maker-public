eb756c688c8f4876e4c72464ca0d612730aa1f1f
Hien Dang <hienpd@outlook.com>
2016-05-14 14:08:47 -0700

    Got the brush colors to work

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 var canvas = document.getElementById('canvas');
 var brushColor;
 
+// Draw the canvas based on the height and width given.
+// Height determines the number of rows.
+// Width determines how many pixels to put in each row.
+
 var drawCanvas = function (width, height) {
   for (var row = 0; row < height; row++) {
     var pixelRow = document.createElement('div');
@@ -12,20 +16,21 @@ var drawCanvas = function (width, height) {
       pixel.className = 'pixel';
       pixelRow.appendChild(pixel);
       pixel.addEventListener('click', function(event) {
-        event.target.className += ' red';
+        // event.target.className += ' red';
+        event.target.className += ' ' + brushColor;
       });
     }
   }
 };
 
-// Pick a color. Click on a color and save the class name
+// Pick a color. Clicking on a color saves the class name.
 
 var colorPicker = function () {
   var palette = document.getElementById('palette');
   var color = palette.getElementsByClassName('color');
   for (var i = 0; i < color.length; i++) {
     color[i].addEventListener('click', function(event) {
-      brushColor = event.target.className;
+      brushColor = event.target.className.slice(6);
     });
   }
 };
