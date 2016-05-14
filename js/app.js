e229ad54f01e39dda2e31acd4eca9d0e70f5f6dd
Hien Dang <hienpd@outlook.com>
2016-05-14 13:00:20 -0700

    Clicking a color saves the color

--- js/app.js
+++ js/app.js
@@ -1,16 +1,13 @@
-// Get the main element to start putting stuff into it
 var canvas = document.getElementById('canvas');
+var brushColor;
 
 var drawCanvas = function (width, height) {
   for (var row = 0; row < height; row++) {
-
     var pixelRow = document.createElement('div');
-
     pixelRow.className = 'pixelRow';
     canvas.appendChild(pixelRow);
 
     for (var i = 0; i < width; i++) {
-
       var pixel = document.createElement('div');
       pixel.className = 'pixel';
       pixelRow.appendChild(pixel);
@@ -19,8 +16,19 @@ var drawCanvas = function (width, height) {
       });
     }
   }
+};
 
+// Pick a color. Click on a color and save the class name
 
+var colorPicker = function () {
+  var palette = document.getElementById('palette');
+  var color = palette.getElementsByClassName('color');
+  for (var i = 0; i < color.length; i++) {
+    color[i].addEventListener('click', function(event) {
+      brushColor = event.target.className;
+    });
+  }
 };
 
 drawCanvas(32, 24);
+colorPicker();
