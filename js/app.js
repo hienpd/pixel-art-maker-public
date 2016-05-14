86a2b26cbe9f6ac8b4ffce7097229e7529b381b0
Hien Dang <hienpd@outlook.com>
2016-05-13 17:01:56 -0700

    Got some red in there

--- js/app.js
+++ js/app.js
@@ -2,17 +2,26 @@
 var canvas = document.getElementById('canvas');
 
 var drawCanvas = function (width, height) {
-  for (var row = 0; row < height; row++) { // Draw height number of rows
+  for (var row = 0; row < height; row++) {
+
     var pixelRow = document.createElement('div');
+
     pixelRow.className = 'pixelRow';
     canvas.appendChild(pixelRow);
-    for (var i = 0; i < width; i++) { // Draw out row this wide (width)
+
+    for (var i = 0; i < width; i++) {
+
       var pixel = document.createElement('div');
+      pixel.className = 'pixel';
       pixelRow.appendChild(pixel);
+      pixel.addEventListener('click', function(event) {
+        event.target.className += ' red';
+      });
+
     }
   }
 
 
 };
 
-drawCanvas(2, 2);
+drawCanvas(32, 24);
