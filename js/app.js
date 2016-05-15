acbf6143a753b0dc17c9cb895c496862826c41a3
Hien Dang <hienpd@outlook.com>
2016-05-15 01:08:43 -0700

    Bonus #1 custom color picker

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
-var canvas = document.getElementById('canvas');
 var brushColor;
+var customColor = '';
+var toggle = true;
+var canvas = document.getElementById('canvas');
+var currentColor = document.getElementsByClassName('current')[0];
 
-/* Draw the canvas based on the height and width given. Height determines the number of rows. Width determines how many pixels to put in each row. */
+/* Draw the canvas based on the height and width given. Height determines the number of rows. Width determines how many pixels to put in each row. Add event listeners to each pixel. Toggle between colorPicker palette (true) and customPicker (false). */
 
 var drawCanvas = function (width, height) {
   for (var row = 0; row < height; row++) {
@@ -14,25 +17,36 @@ var drawCanvas = function (width, height) {
       pixel.className = 'pixel';
       pixelRow.appendChild(pixel);
       pixel.addEventListener('click', function(event) {
-        if (event.target.className.length > 5) {
-          event.target.className = 'pixel ' + brushColor;
-        } else {
-          event.target.className += ' ' + brushColor;
+
+        switch (toggle) {
+          case false:
+            event.target.className = 'pixel';
+            event.target.style.backgroundColor = customColor;
+            event.target.style.borderColor = customColor;
+            break;
+          case true:
+            if (event.target.className.length > 5) {
+              event.target.className = 'pixel ' + brushColor;
+            } else {
+              event.target.removeAttribute('style');
+              event.target.className += ' ' + brushColor;
+            }
         }
       });
     }
   }
 };
 
-/* Allow user to pick a color. Clicking on a color saves the class name. Add event listeners to all the color panels. Add brush color class to indicator. */
+/* Allow user to pick a color. Clicking on a color saves the class name. Add event listeners to all the color panels. Show brush color on indicator. */
 
 var colorPicker = function () {
   var palette = document.getElementById('palette');
   var color = palette.getElementsByClassName('color');
-  var currentColor = document.getElementsByClassName('current')[0];
   for (var i = 0; i < color.length; i++) {
     color[i].addEventListener('click', function(event) {
     brushColor = event.target.className.slice(13);
+    currentColor.removeAttribute('style');
+    toggle = true;
     if (brushColor === 'erase') {
       currentColor.className = 'color current gridbg ' + brushColor;
     } else {
@@ -42,5 +56,14 @@ var colorPicker = function () {
   }
 };
 
+var customPicker = function (event) {
+  customColor = event.target.value;
+
+  currentColor.className = 'color current shadow';
+  currentColor.style.backgroundColor = customColor;
+  toggle = false;
+};
+
 drawCanvas(55, 30);
 colorPicker();
+customPicker();
